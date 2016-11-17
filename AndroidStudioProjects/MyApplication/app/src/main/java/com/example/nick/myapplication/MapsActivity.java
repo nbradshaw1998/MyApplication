0b8718621c585718a9581afd6139816f9b950d34
Nick Bradshaw <nbradshaw1998@gmail.com>
2016-11-17 09:53:31 -0500

    Update MapsActivity.java

--- AndroidStudioProjects/MyApplication/app/src/main/java/com/example/nick/myapplication/MapsActivity.java
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/example/nick/myapplication/MapsActivity.java
@@ -1,34 +1,39 @@
 package com.example.nick.myapplication;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import android.telephony.gsm.SmsMessage;
+import android.widget.Toast;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+/**
+ * Created by Nick on 11/7/2016.
+ */
 
-    private GoogleMap mMap;
-
+public class SmsReceiver extends BroadcastReceiver {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
+    public void onReceive(Context context, Intent intent) {
+        //---get the SMS message passed in---
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] msgs = null;
+        String messageReceived = "";
+        if (bundle != null) {
+            //---retrieve the SMS message received---
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            msgs = new SmsMessage[pdus.length];
+            for (int i = 0; i < msgs.length; i++)
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            {
+                msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                messageReceived += msgs[i].getMessageBody().toString();
+                messageReceived += "\n";
+               try{
+                Toast.makeText(context, messageReceived, Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            }
+        }
     }
 }
